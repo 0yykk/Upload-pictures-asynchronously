156eaa1e9e55ed111d650108ede58f981ed988ca
0yykk <921122271@qq.com>
2020-01-02 16:05:04 +0800

    example

--- Upload-pictures-asynchronously/App_Start/RouteConfig.cs
+++ Upload-pictures-asynchronously/App_Start/RouteConfig.cs
@@ -13,10 +13,15 @@ namespace Upload_pictures_asynchronously
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "FileOperation", action = "ImageUpload", id = UrlParameter.Optional }
             );
         }
     }
---
+++ Upload-pictures-asynchronously/Controllers/FileOperationController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Upload_pictures_asynchronously.Controllers
+{
+    public class FileOperationController : Controller
+    {
+        // GET: FileOperation
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ImageUpload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ProcessImgUpload()
+        {
+            var file = Request.Files["imgFile"];
+            string path = "/Upload/" + Guid.NewGuid().ToString() + file.FileName;
+            string savepath = Request.MapPath(path);
+            file.SaveAs(savepath);
+            return Content(path);
+        }
+
+    }
+}
